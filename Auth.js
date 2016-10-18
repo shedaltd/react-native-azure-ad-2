df7554a4226b72b977226ee4334cdf453d2a6f68
Max <maxzitron@gmail.com>
2016-10-18 11:23:20 +1100

    Initail commit

---
+++ Auth.js
@@ -0,0 +1,139 @@
+import uuid from 'uuid';
+import querystring from 'query-string';
+import URL from 'url';
+import {
+    Buffer
+} from 'buffer';
+
+export default class Auth {
+
+    constructor(instance) {
+        const config = instance.getConfig();
+
+        this.authority = config.authority;
+        this.authorize_endpoint = config.authorize_endpoint;
+        this.token_endpoint = config.token_endpoint;
+        this.client_id = config.client_id;
+        this.client_secret = config.client_secret;
+        this.redirect_uri = config.redirect_uri;
+        this.scope = config.scope;
+
+        // function binding
+        this.getAuthUrl = this.getAuthUrl.bind(this);
+        this._request = this._request.bind(this);
+    }
+
+    getAuthUrl() {
+        return this.authority + this.authorize_endpoint +
+            '?client_id=' + this.client_id +
+            '&response_type=code' +
+            '&redirect_uri=' + this.redirect_uri +
+            '&scope=' + this.scope +
+            '&response_mode=query' +
+            '&nonce=' + uuid.v4() +
+            '&state=abcd';
+    }
+
+    _request(params: any): Promise {
+        const post_data = querystring.stringify(params);
+
+        // create request endpoint
+        const endpoint = this.authority + this.token_endpoint;
+
+        // set port based on http protocol
+        var parsedUrl = URL.parse(endpoint, true);
+        if (parsedUrl.protocol == "https:" && !parsedUrl.port) {
+            parsedUrl.port = 443;
+        }
+
+        // set request header
+        var realHeaders = {};
+        realHeaders['Host'] = parsedUrl.host;
+
+        var queryStr = querystring.stringify(parsedUrl.query);
+        if (queryStr) queryStr = "?" + queryStr;
+
+        if (post_data) {
+            if (Buffer.isBuffer(post_data)) {
+                realHeaders["Content-Length"] = post_data.length;
+            } else {
+                realHeaders["Content-Length"] = Buffer.byteLength(post_data);
+            }
+        } else {
+            realHeaders["Content-length"] = 0;
+        }
+
+        realHeaders["Content-Type"] = 'application/x-www-form-urlencoded';
+
+        // create request option object
+        const options = {
+            host: parsedUrl.hostname,
+            port: parsedUrl.port,
+            path: parsedUrl.pathname + queryStr,
+            method: "POST",
+            headers: realHeaders
+        };
+
+        const payload = Object.assign({
+            body: post_data
+        }, options);
+
+        // request token
+        return new Promise((resolve, reject) => {
+            // fetch access_token
+            fetch(endpoint, payload)
+                .then(response => {
+                    // return blob object
+                    return response.blob()
+                })
+                .then(response => {
+                    // read blob object back to json
+                    var reader = new FileReader();
+                    reader.readAsText(response);
+                    reader.onload = function(event) {
+                        var result = JSON.parse(event.target.result);
+                        // resolve promise
+                        resolve({
+                            accessToken: result.access_token,
+                            refreshToken: result.refresh_token
+                        });
+                    };
+                }).catch(err => {
+                    // incase of error reject promise
+                    reject(err);
+                });
+        });
+
+
+    }
+
+    getTokenFromCode(code: string): Promise {
+        var params = {
+            client_id: this.client_id,
+            client_secret: this.client_secret,
+            code: code,
+            grant_type: 'authorization_code',
+            redirect_uri: this.redirect_uri,
+            response_mode: 'form_post',
+            nonce: uuid.v4(),
+            state: 'abcd'
+        }
+
+        return this._request(params);
+    }
+
+    getTokenFromRefreshToken(refreshToken: string): Promise {
+        var params = {
+            client_id: this.client_id,
+            client_secret: this.client_secret,
+            refresh_token: code,
+            grant_type: 'refresh_token',
+            redirect_uri: this.redirect_uri,
+            response_mode: 'form_post',
+            nonce: uuid.v4(),
+            state: 'abcd'
+        }
+
+        return this._request(params);
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import AzureInstance from './AzureInstance';
+import Auth from './Auth';
+import AzureLoginView from './AzureLoginView';
+
+export {
+  AzureInstance, Auth, AzureLoginView
+}
