d23c22de08cc7049b17a550196d7db4cb810f592
Max <maxzitron@users.noreply.github.com>
2018-10-30 10:26:44 +1100

    style: replace var with const

--- example/App.js
+++ example/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import {AzureInstance, AzureLoginView} from 'react-native-azure-ad-2';
 
-var credentials = {
+const credentials = {
   client_id: '2fa55851-89e0-4ccf-8b86-2a682bdb13d9',
   client_secret: 'prJaOuSFMppLGa9E2bCScRR',
   scope: 'User.ReadBasic.All User.Read.All User.ReadWrite.All Mail.Read offline_access'
 };
 
 export default class App extends React.Component {
-
   constructor(props){
     super(props);
     
