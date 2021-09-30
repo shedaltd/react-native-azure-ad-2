077a11d0943248bb6f2e5fb5ba645213dfe6ba29
Mike Ebinum <mebinum@gmail.com>
2021-09-30 14:48:19 +1000

    added new example project

--- example/App.js
+++ example/App.js
@@ -1,57 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {AzureInstance, AzureLoginView} from 'react-native-azure-ad-2';
 
-const credentials = {
-  client_id: '2fa55851-89e0-4ccf-8b86-2a682bdb13d9',
-  client_secret: 'prJaOuSFMppLGa9E2bCScRR',
-  scope: 'User.ReadBasic.All User.Read.All User.ReadWrite.All Mail.Read offline_access'
-};
-
-export default class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      azureLoginObject: {},
-      loginSuccess: false
-    };
-		this.azureInstance = new AzureInstance(credentials);
-		this._onLoginSuccess = this._onLoginSuccess.bind(this);
-	}
-
-	_onLoginSuccess(){
-		this.azureInstance.getUserInfo().then(result => {
-			this.setState({
-        loginSuccess: true,
-        azureLoginObject: result
-      });
-      console.log(result);
-		}).catch(err => {
-			console.log(err);
-		})
-  }
-  
-
-  render() {
-
-    if (!this.state.loginSuccess) {
-
-      return (<AzureLoginView
-          azureInstance={this.azureInstance}
-          loadingMessage="Requesting access token"
-          onSuccess={this._onLoginSuccess}
-        />)
-    }
-
-    const {userPrincipalName, givenName} = this.state.azureLoginObject;
-    
-    return (
-      <View style={styles.container}>
-				<Text style={styles.text}>Welcome {givenName}</Text> 
-				<Text style={styles.text}>You logged into Azure with {userPrincipalName}</Text> 
-			</View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -61,9 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-		textAlign: 'center',
-		color: '#333333',
-		marginBottom: 5
-	}
 });
--- example/README.md
+++ example/README.md
@@ -3,12 +3,33 @@
 This is an example created to demonstrate a basic integration of `REACT-NATIVE-AZURE-AD2`
 
 To start you first have to install dependencies
+
 ```bash
-$ npm install
+  yarn install
 ```
 
-Then you will only need to run
+## iOS
+
+To run on iOS you will need to run
+
 ```bash
-$ expo start
+    yarn ios
 ```
+
 and follow the instruction in your terminal
+
+## Issues
+
+if you get an error similar to the one below
+
+``` bash
+Invariant Violation: requireNativeComponent: "RNCSafeAreaProvider" was not found in the UIManager 
+```
+
+run
+
+```bash
+    npx pod-install ios
+```
+
+You might have clear the data in your simulator i.e. `Devices > Erase All Content and Settings` and try again.
